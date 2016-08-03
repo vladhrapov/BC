a8bd5b6c976e578f95e72862dd7a26978b5e64fb
Vladyslav Khrapov <Vladyslav Khrapov>
2016-08-03 23:49:31 +0300

    Add UnitTest project

---
+++ BC.UnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BC.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(true, true);
+        }
+    }
+}
